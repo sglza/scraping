df68ec20e2ee85b23b18fa2d216ff22b7a801770
santiago <santiago.g.angeles@gmail.com>
2019-10-07 19:19:24 -0500

    first branch commit

--- playerSoup.py
+++ playerSoup.py
@@ -13,7 +13,7 @@ def getPlayerHitting():
     f = open(fileName, 'w')
     headers = 'Rank, Player, Team, Position, Games, At Bats, Runs, Hits, Doubles, Triples, Home Runs, Runs Batted, Walks, Strikeout, Bases Stolen, Caught Stealing, Avg. Hits (hits/at bats), On-base %, Slugging %, O+S\n'
     f.write(headers)
-
+ 
     my_url = "http://mlb.mlb.com/stats/sortable_es.jsp#elem=%5Bobject+Object%5D&tab_level=child&click_text=Sortable+Player+hitting&game_type='R'&season=2018&season_type=ANY&league_code='MLB'&sectionType=sp&statType=hitting&page=1&ts=1569359008122&playerType=QUALIFIER&sportCode='mlb'&split=&team_id=&active_sw=&position=&page_type=SortablePlayer&sortOrder='desc'&sortColumn=avg&results=&perPage=50&timeframe=&last_x_days=&extended=0"
  
     browser = webdriver.Firefox()
